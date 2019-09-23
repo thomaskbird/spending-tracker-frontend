5543cae5574acb3b3a6968c100df1aeb40f38d1f
Tom Bird <thomas.bird1984@gmail.com>
2019-09-22 20:40:42 -0400

    Reformatting data

--- src/components/charts/LineChartWrapper.tsx
+++ src/components/charts/LineChartWrapper.tsx
@@ -13,13 +13,8 @@ import {
 } from "recharts";
 
 const data = [
-    {name: "Page A", uv: 4000, pv: 2400, amt: 2400},
-    {name: "Page B", uv: 3000, pv: 1398, amt: 2210},
-    {name: "Page C", uv: 2000, pv: 9800, amt: 2290},
-    {name: "Page D", uv: 2780, pv: 3908, amt: 2000},
-    {name: "Page E", uv: 1890, pv: 4800, amt: 2181},
-    {name: "Page F", uv: 2390, pv: 3800, amt: 2500},
-    {name: "Page G", uv: 3490, pv: 4300, amt: 2100},
+    {month: "September", inc: 2400, exp: 1347},
+    {month: "August", inc: 2210, exp: 1000},
 ];
 
 interface Props {
@@ -45,13 +40,13 @@ export class LineChartWrapper extends React.Component<Props, State> {
                     data={data}
                     margin={{top: 5, right: 30, left: 20, bottom: 5}}
                 >
-                    <XAxis dataKey="name"/>
+                    <XAxis dataKey="month"/>
                     <YAxis/>
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip/>
                     <Legend />
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="inc" stroke="#8884d8" activeDot={{r: 8}}/>
+                    <Line type="monotone" dataKey="exp" stroke="#82ca9d" />
                 </LineChart>
             </ResponsiveContainer>
         );
